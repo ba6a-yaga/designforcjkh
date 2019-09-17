c1a8b0f45eb2e5c65671124e7bbd02fa90e3cdc5
x3mprog <x3mprog@gmail.com>
2019-09-17 19:28:59 +0300

    add docs

---
+++ docs/precache-manifest.af019cd25769d673092b9794a695d5c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb3dfbc1ef03aab0c6d9128fe6a1db61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5ffb81d290079af6d07",
+    "url": "/static/css/main.74743332.chunk.css"
+  },
+  {
+    "revision": "0124e8e4476df2d6281f",
+    "url": "/static/js/2.b7b101fb.chunk.js"
+  },
+  {
+    "revision": "c5ffb81d290079af6d07",
+    "url": "/static/js/main.116ed40c.chunk.js"
+  },
+  {
+    "revision": "d84796056283f355ec35",
+    "url": "/static/js/runtime~main.237b8957.js"
+  },
+  {
+    "revision": "6af61e1d27f6730adefe6362e1d9693d",
+    "url": "/static/media/index.6af61e1d.png"
+  }
+]);
